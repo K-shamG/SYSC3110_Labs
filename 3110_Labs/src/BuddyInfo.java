db11d74680dadba9547670d4f5d724b336ebbb82
Kshamina <kshamina.ghelani@carleton.ca>
2016-12-17 17:44:52 -0500

    Lab 1

--- 3110_Labs/src/BuddyInfo.java
+++ 3110_Labs/src/BuddyInfo.java
@@ -2,5 +2,47 @@
 public class BuddyInfo {
 	private String name; 
 	private String address;
-	private String number; 
+	private String number;
+	
+	public BuddyInfo() {
+		this(null, null, null);
+	}
+	
+	public BuddyInfo(String name, String address, String number) {
+		this.name = name;
+		this.address = address;
+		this.number = number;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String toString() {
+		return "Hello " + name + "!"; 
+	}
+	
+	public static void main(String[] args) {
+		String name = "Kshamina";
+		
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName(name);
+		System.out.println(buddy.toString());
+				
+	}
 }
